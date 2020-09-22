81e57cb83c08b780ea71f76c79ee5684d4648b9e
Ivan Stana <^_^@myrtana.sk>
2020-09-22 21:11:31 +0200

    Initialize gem

---
+++ lib/nevergonnagiveyouup.rb
@@ -0,0 +1,6 @@
+require "nevergonnagiveyouup/version"
+
+module Nevergonnagiveyouup
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/nevergonnagiveyouup/version.rb
@@ -0,0 +1,3 @@
+module Nevergonnagiveyouup
+  VERSION = "0.1.0"
+end
